ce08283d43c5282c94942692642bd77b1155146f
RINKU KUMAR <145314905+Mr-Rinku-Kumar@users.noreply.github.com>
2025-02-20 05:03:23 +0530

    Update ToDo.tsx

--- client/src/components/ToDo.tsx
+++ client/src/components/ToDo.tsx
@@ -2,27 +2,28 @@ import { baseURL } from "@/utils/constant";
 import axios from "axios";
 import { MdDelete } from "react-icons/md";
 
-interface propsType {
+interface PropsType {
   id: number;
   text: string;
-  setUpdateUI: any;
+  setUpdateUI: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const ToDo = ({ id, text, setUpdateUI }: propsType) => {
+const ToDo = ({ id, text, setUpdateUI }: PropsType) => {
   const deleteToDos = () => {
     axios
       .delete(`${baseURL}/delete/${id}`)
       .then((res) => {
         console.log(res.data);
-        setUpdateUI((preState: any) => !preState);
+        setUpdateUI((prevState) => !prevState);
       })
       .catch((err) => console.log(err));
   };
+
   return (
     <div className="bg-accentLight p-4 text-gray-400 relative group">
       {text}
       <div
-        className="hidden absolute top[50%] translate-y-[-50%] right-0 mx-4 group-hover:block cursor-pointer"
+        className="hidden absolute top-1/2 -translate-y-1/2 right-0 mx-4 group-hover:block cursor-pointer"
         onClick={deleteToDos}
       >
         <MdDelete />
